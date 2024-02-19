8acd0d88f20895633c9b15ea79b164bf57f77ff4
calebe1234 <142435578+calebe1234@users.noreply.github.com>
2024-02-19 12:23:02 -0300

    Update dowhile.js

--- dowhile.js
+++ dowhile.js
@@ -1,4 +1,4 @@
-var natriz = ["vetor1" , "vetor2" , "vetor3" , "vetor4" , "vetor5"];
+var matriz = ["vetor1" , "vetor2" , "vetor3" , "vetor4" , "vetor5"];
 var posicao =0;
 
 while (posicao < natriz.length) {
